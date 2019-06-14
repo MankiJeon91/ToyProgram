13cc25bedfa25b07f7e326380060e1fe6384d9b5
MankiJeon91 <gracesong_jdi@naver.com>
2019-06-14 16:56:12 +0900

    JSP를 이용하여 servlet에서 view부분 분리 - 회원 조회 부분

--- src/spms/servlets/MemberListServlet.java
+++ src/spms/servlets/MemberListServlet.java
@@ -1,18 +1,22 @@
 package spms.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import spms.vo.Member;
 
 @WebServlet("/member/list")
 public class MemberListServlet extends HttpServlet {
@@ -31,19 +35,34 @@ public class MemberListServlet extends HttpServlet {
 			rs = stmt.executeQuery("SELECT MNO, MNAME, EMAIL, CRE_DATE FROM MEMBERS ORDER BY MNO ASC");
 
 			response.setContentType("text/html;charset=UTF-8");
-			PrintWriter writer = response.getWriter();
-			writer.println("<html><head><title>회원목록</title></head>");
-			writer.println("<body><h1>회원목록</h1>");
 			
-			writer.println("<p><a href='add'>신규 회원</a></p>");
+			List<Member> members = new ArrayList<Member>();
 			
-			while (rs.next()) {
-				writer.println(rs.getInt("MNO") + "," +
-						"<a href='update?no=" + rs.getInt("MNO") + "'>" + rs.getString("MNAME") + "</a>,"	//<a href='update?no=1'>홍길동</a>형식 
-						+ rs.getString("EMAIL") + ","
-						+ rs.getDate("CRE_DATE") + "<a href='delete?no=" + rs.getInt("MNO") + "'>[삭제]</a><br>");
+			while(rs.next()) {
+				members.add(new Member().setNo(rs.getInt("MNO")).setName(rs.getString("MNAME")).setEmail(rs.getString("EMAIL")).setCreatedDate(rs.getDate("CRE_DATE")));
 			}
-			writer.println("</body></html>");
+			
+			//request에 회원 목록 데이터를 보관한다.
+			request.setAttribute("members", members);
+			
+			//JSP로 출력을 위임한다.
+			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberList.jsp");
+			//이제 해당 JSP와 serlvet은 request와 response를 공유한다.
+			rd.include(request, response);
+			
+			/* JSP를 적용하기 위해 삭제
+			 * PrintWriter writer = response.getWriter();
+			 * writer.println("<html><head><title>회원목록</title></head>");
+			 * writer.println("<body><h1>회원목록</h1>");
+			 * 
+			 * writer.println("<p><a href='add'>신규 회원</a></p>");
+			 * 
+			 * while (rs.next()) { writer.println(rs.getInt("MNO") + "," +
+			 * "<a href='update?no=" + rs.getInt("MNO") + "'>" + rs.getString("MNAME") +
+			 * "</a>," //<a href='update?no=1'>홍길동</a>형식 + rs.getString("EMAIL") + "," +
+			 * rs.getDate("CRE_DATE") + "<a href='delete?no=" + rs.getInt("MNO") +
+			 * "'>[삭제]</a><br>"); } writer.println("</body></html>");
+			 */
 		} catch (Exception e) {
 			throw new ServletException(e);
 		} finally {
